067861df46594327223db7dd5268f4ccf403151e
philliab13 <philliab@stud.ntnu.no>
2025-03-25 15:49:35 +0100

    Started with wrapper, feature selec method - forward_selection to find the best 4th feature to use.

--- Task3.py
+++ Task3.py
@@ -18,4 +18,53 @@
 # MSW
 # F=MSB/MSW.
 
+
+
 #Should probably also look in the book if there is a way they are choosing features, and use the same method. 
+
+#Wrapper methods: There are many different wrapper methods, one can use backaward selection, which starts with all the different features and eliminates one after another to increase the accuracy. This is computantionally heavy,  therefore one could try forward selection instead where I can start with the 3 features already selected and add 1 after another features and see which makes the accuracy the highest. 
+from Task_1 import *
+
+def forward_selection(base_features,candidate_features,performance_metric,data):
+    performance_history=[]
+    rem_feat=candidate_features
+    sel_feat=base_features
+
+    best_score=0
+    improvment=True
+
+
+    while improvment and rem_feat:
+        improvment=False
+        scores_candidates=[]
+
+        for feature in rem_feat:
+
+
+            temp_feat=sel_feat+ candidate_features[-1] 
+            temp_feat_with_label=sel_feat+ candidate_features[-1] + "Track ID"+ "GenreID"
+            feat_dict=get_features_dict(data, temp_feat_with_label)
+
+            training_data, training_labels, testing_data, testing_labels=create_data(feat_dict, temp_feat, "GenreID", 0.8)
+            
+
+            #Get the feature matrix with featues and values for each
+
+            #Do score validation
+        best_candidate, score= max(scores, lambda)
+
+        if score > best_score:
+            best_score = score
+            sel_feat.append(best_candidate)
+            rem_feat.remove(best_candidate)
+            improvement = True
+            performance_history.append((list(sel_feat), best_score))
+            print(f"Added feature {best_candidate}, new score: {best_score:.4f}")
+        else:
+            # No improvement, stop selection.
+            break
+
+    return sel_feat, performance_history
+
+
+
